b0133c98f904939f9f4dae4e2434687e0bc925d4
chris <chris.mcclanahan@accelereyes.com>
2011-10-01 00:28:26 -0400

    updated versus.cpp with jkt separable conv2 example

--- cuda/versus/versus.cpp
+++ cuda/versus/versus.cpp
@@ -23,12 +23,19 @@ using namespace cv;
 using namespace gpu;
 
 
-const float h_sobel_kernel[] = { -2.0, -1.0,  0.0,
-                                 -1.0,  0.0,  1.0,
-                                 0.0,  1.0,  2.0
-                               };
-f32 sobel_k = f32(h_sobel_kernel, 3, 3);
-
+const int ksz = 3;
+const float jktmk1[] = {
+    1.0, 2.0, 1.0,
+};
+const float jktmk2[] = {
+    1.0, 0.0, -1.0,
+};
+float cvmk[ksz][ksz] = {
+    {-2, -1,  0, },
+    {-1,  0,  1, },
+    { 0,  1,  2, },
+};
+Mat cvk = Mat (ksz, ksz, CV_32F, cvmk);
 
 void gen(Mat& mat, int rows, int cols, int type, Scalar low, Scalar high) {
     mat.create(rows, cols, type);
@@ -36,12 +43,13 @@ void gen(Mat& mat, int rows, int cols, int type, Scalar low, Scalar high) {
     rng.fill(mat, RNG::UNIFORM, low, high);
 }
 
+const int use_cv_sobel = 1;
 
 int main() {
 
     // setup
     Mat src, dst;
-    GpuMat d_src, d_dst;
+    GpuMat d_src, d_dst, d_cvk;
     int runs = 100;
 
     // bench
@@ -57,20 +65,29 @@ int main() {
                 dst.create(size, size, CV_32FC1);
                 d_src = src;
                 d_dst.create(size, size, CV_32FC1);
+                d_cvk = cvk;
 
-                // cpu
-                Sobel(src, dst, dst.depth(), 1, 1);
-                start_timer(0);
-                for (int i = 0; i < runs; ++i) {
-                    Sobel(src, dst, dst.depth(), 1, 1);
-                }
-                cout  << "  cv-cpu: " << elapsed_time(0) / (float)runs << endl;
+                // // cpu
+                // Sobel(src, dst, dst.depth(), 1, 1);
+                // start_timer(0);
+                // for (int i = 0; i < runs; ++i) {
+                //     Sobel(src, dst, dst.depth(), 1, 1);
+                // }
+                // cout  << "  cv-cpu: " << elapsed_time(0) / (float)runs << endl;
 
                 // gpu
-                gpu::Sobel(d_src, d_dst, d_dst.depth(), 1, 1);
-                start_timer(1);
-                for (int i = 0; i < runs; ++i) {
-                    gpu::Sobel(d_src, d_dst, d_dst.depth(), 1, 1);
+                if (use_cv_sobel) {
+                    gpu::Sobel(d_src, d_dst, d_dst.depth(), 1, 1, ksz);
+                    start_timer(1);
+                    for (int i = 0; i < runs; ++i) {
+                        gpu::Sobel(d_src, d_dst, d_dst.depth(), 1, 1, ksz);
+                    }
+                } else {
+                    convolve(d_src, d_cvk, d_dst);
+                    start_timer(1);
+                    for (int i = 0; i < runs; ++i) {
+                        convolve(d_src, d_cvk, d_dst);
+                    }
                 }
                 cout << "  cv-gpu: " << elapsed_time(1) / (float)runs << endl;
             }
@@ -82,13 +99,15 @@ int main() {
                 src.convertTo(jimg, CV_32FC1);
                 float* fgray = (float*)jimg.data;
                 f32 I1 = f32(fgray, jimg.rows, jimg.cols);
-
+                unsigned dimsb[] = {ksz, ksz};
                 // gpu
-                f32 jdst = conv2(I1, sobel_k, jktConvSame);
+                //f32 jdst = conv2(I1, sobel_k, jktConvSame);
+                f32 jdst = conv2(ksz, jktmk1, ksz, jktmk2, I1, jktConvSame);
                 gsync();
                 start_timer(2);
                 for (int i = 0; i < runs; ++i) {
-                    jdst = conv2(I1, sobel_k, jktConvSame);
+                    //jdst = conv2(I1, sobel_k, jktConvSame);
+                    jdst = conv2(ksz, jktmk1, ksz, jktmk2, I1, jktConvSame);
                     gsync(); // disables smart caching
                 }
                 cout << "  jacket: " << elapsed_time(2) / (float)runs << endl;
